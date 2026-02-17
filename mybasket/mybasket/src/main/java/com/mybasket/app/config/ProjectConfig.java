1d9174e7a6c57931556d09584dbe6b2468cf7311
batchlcwd <batchlcwd@gmail.com>
2026-02-17 22:06:19 +0530

    cors configuration

--- mybasket/mybasket/src/main/java/com/mybasket/app/config/ProjectConfig.java
+++ mybasket/mybasket/src/main/java/com/mybasket/app/config/ProjectConfig.java
@@ -1,9 +1,15 @@
 package com.mybasket.app.config;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class ProjectConfig {
@@ -12,5 +18,7 @@ public class ProjectConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+
 
 }
--- mybasket/mybasket/src/main/java/com/mybasket/app/config/SecurityConfig.java
+++ mybasket/mybasket/src/main/java/com/mybasket/app/config/SecurityConfig.java
@@ -24,6 +24,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -49,6 +54,7 @@ public class SecurityConfig {
         //customization:
 
         httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         requests ->
@@ -85,5 +91,29 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
         return configuration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        //registering configurations
+        corsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+
+        return corsConfigurationSource;
+
+
     }
 }
